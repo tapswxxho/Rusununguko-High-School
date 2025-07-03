46b7fc46e513bd353b7f5340c13c363927cc7d5b
tapswxxho <tapfumaneyianotidaishe09@gmail.com>
2025-07-03 11:36:31 +0200

    Removed Mobile-Friendly navbar

--- js/app.js
+++ js/app.js
@@ -1,10 +1,3 @@
   function toggleNews() {
     const popup = document.getElementById('news-popup');
     popup.style.display = (popup.style.display === 'none' || popup.style.display === '') ? 'block' : 'none';}
-    function toggleContact() {
-    const popup = document.getElementById('contact-popup');
-    popup.style.display = (popup.style.display === 'none' || popup.style.display === '') ? 'block' : 'none';}
-    function toggleMenu() {
-  const nav = document.querySelector('.navitems');
-  nav.classList.toggle('show');
-}

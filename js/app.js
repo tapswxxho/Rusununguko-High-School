0dddf3e4745ad8238d6317bf38050745865eb392
tapswxxho <tapfumaneyianotidaishe09@gmail.com>
2025-07-11 19:46:27 +0200

    vercel insights

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
   function toggleNews() {
     const popup = document.getElementById('news-popup');
     popup.style.display = (popup.style.display === 'none' || popup.style.display === '') ? 'block' : 'none';}
+  import { SpeedInsights } from '@vercel/speed-insights/next';
